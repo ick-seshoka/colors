efb9cf3d9607c8a2a1cf2c3ad63c1095f850f31c
Isaac <isaac@valr.com>
2021-02-16 21:34:15 +0200

    add error handling to copy color

--- src/helpers/copyColor.js
+++ src/helpers/copyColor.js
@@ -1,12 +1,19 @@
 export function copyColorToClipboard(colorCode) {
-  navigator.clipboard.writeText(colorCode).then(
-    function () {
-      /* clipboard successfully set */
-      console.log("Copied " + colorCode);
-    },
-    function () {
-      /* clipboard write failed */
-      console.log("Went south copying " + colorCode);
-    }
-  );
+  try {
+    navigator.clipboard.writeText(colorCode).then(
+      function () {
+        /* clipboard successfully set */
+        console.log("Copied " + colorCode);
+      },
+      function () {
+        /* clipboard write failed */
+        console.log("Went south copying " + colorCode);
+      }
+    );
+  } catch (e) {
+    /*
+     * Add sentry for error logging
+     */
+    console.log("Copy failed", e);
+  }
 }
